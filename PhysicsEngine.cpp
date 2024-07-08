eee8cfa9974da23377a0331bc18644435cb9cc80
MunHwaDong <coming2430@gmail.com>
2024-07-08 17:50:10 +0900

    Update PhysicsEngine.cpp
    초기 중력 구현

--- PhysicsEngine.cpp
+++ PhysicsEngine.cpp
@@ -1,11 +1,47 @@
 #include "PhysicsEngine.h"
 
-void PhysicsEngine::Update(const float deltaTime)
+void PhysicsEngine::GenerateForce(const float deltaTime)
+{
+	for (auto& obj : objects)
+	{
+		//test
+		if (obj.shape.vertices[1].y <= -1 && obj.shape.vertices[2].y <= 1)
+		{
+			break;
+		}
+
+		obj.vel += (C_gravitycoeff * obj.mass) * deltaTime;
+
+		obj.pos += obj.vel * deltaTime;
+
+		std::cout << obj.pos.x << " " << obj.pos.y << std::endl;
+	}
+}
+
+void PhysicsEngine::RigidbodyUpdate(const float deltaTime)
+{
+	for (auto& obj : objects)
+	{
+		obj.UpdateVertices();
+	}
+}
+
+void PhysicsEngine::DetectCollision()
+{
+
+}
+void PhysicsEngine::ResolutionCollision()
 {
 
 }
 
+void PhysicsEngine::Update(const float deltaTime)
+{
+	GenerateForce(deltaTime);
+	RigidbodyUpdate(deltaTime);
+}
+
 void PhysicsEngine::AddObject(const RenderableObject& object)
 {
-
+	objects.push_back(object);
 }
