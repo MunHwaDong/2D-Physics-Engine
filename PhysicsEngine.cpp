d00d078c1e3c29c1b08953b7ceb01aae483539bd
MunHwaDong <coming2430@gmail.com>
2024-07-29 20:24:51 +0900

    Iterator class 구현

--- PhysicsEngine.cpp
+++ PhysicsEngine.cpp
@@ -3,26 +3,24 @@
 //��� ������Ʈ�鿡 �߷��� ���Ѵ�.
 void PhysicsEngine::GenerateForce(const float deltaTime)
 {
-	for (auto& obj : objects)
+	for (auto ele : objects)
 	{
-		//test
-		if (obj->shape->vertices[1].y <= -1 && obj->shape->vertices[2].y <= 1)
-		{
-			break;
-		}
+		RenderableObject* obj = dynamic_cast<RenderableObject*>(ele);
 
 		obj->vel += (C_gravitycoeff * obj->inverseMass) * deltaTime;
 		obj->pos += obj->vel * deltaTime;
 
-		std::cout << "obj �ӵ� : " << obj->vel.y << std::endl;
+		std::cout << "obj 속도 : " << obj->vel.y << std::endl;
 		std::cout << obj->pos.x << " " << obj->pos.y << std::endl;
 	}
 }
 
 void PhysicsEngine::RigidbodyUpdate(const float deltaTime)
 {
-	for (auto& obj : objects)
+	for (auto ele : objects)
 	{
+		RenderableObject* obj = dynamic_cast<RenderableObject*>(ele);
+
 		obj->UpdateVertices();
 	}
 }
@@ -46,5 +44,5 @@ void PhysicsEngine::Update(const float deltaTime)
 
 void PhysicsEngine::AddObject(RenderableObject* object)
 {
-	objects.push_back(object);
+	objects.Insert(dynamic_cast<IQTData*>(object));
 }
